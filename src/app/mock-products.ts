3c7858afee1642dfd0a3697a12e3ad4405c063b3
Jose Garcia <jogarcia@tuta.io>
2022-03-07 11:13:53 -0500

    Make products mock

---
+++ src/app/mock-products.ts
@@ -0,0 +1,21 @@
+import { Product } from './product';
+
+export const FEATURED_PRODUCTS: Product[] = [
+  { id: 1, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 2, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 3, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 4, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+];
+
+export const PRODUCTS: Product[] = [
+  { id: 1, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 2, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 3, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 4, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 5, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 6, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 7, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 8, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 9, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+  { id: 10, name: 'Dr Nice', description: 'Lorem ipsum', price: '20' },
+];
