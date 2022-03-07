21daebfbb988626a98fdee18fc11f744b21802af
Jose Garcia <jogarcia@tuta.io>
2022-03-07 08:59:56 -0500

    Add and use store-fron component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreFrontComponent } from './store-front/store-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreFrontComponent
   ],
   imports: [
     BrowserModule,
