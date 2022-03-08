5214063a3ba71a7d6790471c13b0acd4a64985cf
Jose Garcia <jogarcia@tuta.io>
2022-03-08 15:33:35 -0500

    Make basic cart view

--- src/app/view-cart/view-cart.component.ts
+++ src/app/view-cart/view-cart.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../product';
+import { CartService } from '../cart.service';
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-view-cart',
   templateUrl: './view-cart.component.html',
-  styleUrls: ['./view-cart.component.scss']
+  styleUrls: ['./view-cart.component.scss'],
 })
 export class ViewCartComponent implements OnInit {
+  cartProducts: Product[] = [];
+  featuredProducts: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getFeaturedProducts();
   }
 
+  getProducts(): void {
+    this.cartProducts = this.cartService.getProducts();
+  }
+
+  getFeaturedProducts(): void {
+    this.featuredProducts = this.productService.getFeaturedProducts();
+  }
 }
