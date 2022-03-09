48dec1a353c380a91bd59b45787a48a51e7d4c25
Jose Garcia <jogarcia@tuta.io>
2022-03-09 15:42:16 -0500

    Add auth paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,18 @@ import { StoreFrontComponent } from './store-front/store-front.component';
 import { ViewCartComponent } from './view-cart/view-cart.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// auth routes
+import { AuthComponent } from './app-auth/auth/auth.component';
+import { ViewLoginComponent } from './app-auth/view-login/view-login.component';
+
 const routes: Routes = [
   { path: '', component: StoreFrontComponent },
   { path: 'cart', component: ViewCartComponent },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [{ path: '', component: ViewLoginComponent }],
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
 
