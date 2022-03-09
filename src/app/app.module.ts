0506fca60a4deec7e2404c1c3db8bbd04a84997a
Jose Garcia <jogarcia@tuta.io>
2022-03-09 12:10:10 -0500

    Import auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
 import { StoreFrontComponent } from './store-front/store-front.component';
 import { StoreFrontNavComponent } from './nav/app-nav.component';
 import { StoreFrontProductsNavComponent } from './store-front-products-nav/store-front-products-nav.component';
-
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { StoreFrontCarouselComponent } from './store-front-carousel/store-front-carousel.component';
 import { StoreFrontFeaturedComponent } from './store-front-featured/store-front-featured.component';
@@ -15,6 +16,8 @@ import { FooterComponent } from './footer/footer.component';
 import { ViewCartComponent } from './view-cart/view-cart.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BaseCardComponent } from './base-card/base-card.component';
+
+import { AppAuthModule } from "./app-auth/app-auth.module";
 
 @NgModule({
   declarations: [
@@ -34,6 +37,7 @@ import { BaseCardComponent } from './base-card/base-card.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     CarouselModule,
+    AppAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent],
