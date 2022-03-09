5a29cb541b43b2edf6080cf44f438184cc24f215
Jose Garcia <jogarcia@tuta.io>
2022-03-09 12:11:52 -0500

    Create basic login component

--- src/app/app-auth/app-auth.module.ts
+++ src/app/app-auth/app-auth.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewLoginComponent } from './view-login/view-login.component';
 import { AuthComponent } from './auth/auth.component';
 
 
 
 @NgModule({
+  declarations: [
+    ViewLoginComponent,
+    AuthComponent
+  ],
   imports: [
     CommonModule
   ]
