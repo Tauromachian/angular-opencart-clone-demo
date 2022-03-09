fa9405250fd7826137638744f22c19172687871f
Jose Garcia <jogarcia@tuta.io>
2022-03-09 15:47:11 -0500

    Add register view

--- src/app/app-auth/app-auth.module.ts
+++ src/app/app-auth/app-auth.module.ts
@@ -4,9 +4,10 @@ import { ViewLoginComponent } from './view-login/view-login.component';
 import { AuthComponent } from './auth/auth.component';
 
 import { RouterModule } from '@angular/router';
+import { ViewRegisterComponent } from './view-register/view-register.component';
 
 @NgModule({
-  declarations: [ViewLoginComponent, AuthComponent],
+  declarations: [ViewLoginComponent, AuthComponent, ViewRegisterComponent],
   imports: [CommonModule, RouterModule],
 })
 export class AppAuthModule {}
