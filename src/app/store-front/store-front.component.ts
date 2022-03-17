99452694926b0772c6376d8da448b8391143e8b2
Jose Garcia <jogarcia@tuta.io>
2022-03-17 16:11:19 -0400

    Import publicly cartService

--- src/app/store-front/store-front.component.ts
+++ src/app/store-front/store-front.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-store-front',
   templateUrl: './store-front.component.html',
-  styleUrls: ['./store-front.component.scss']
+  styleUrls: ['./store-front.component.scss'],
 })
 export class StoreFrontComponent implements OnInit {
+  constructor(public cartService: CartService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
