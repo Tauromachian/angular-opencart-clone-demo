1200cdeb7dc51d77eec03d5825089ecc26c405b3
Jose Garcia <jogarcia@tuta.io>
2022-03-17 17:06:42 -0400

    Import and inject cartService

--- src/app/nav/app-nav.component.ts
+++ src/app/nav/app-nav.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './app-nav.component.html',
   styleUrls: ['./app-nav.component.scss'],
 })
 export class StoreFrontNavComponent implements OnInit {
-  constructor() {}
+  constructor(public cartService: CartService) {}
 
   ngOnInit(): void {}
 }
