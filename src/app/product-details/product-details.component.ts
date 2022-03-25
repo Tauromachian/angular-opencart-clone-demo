fe720b1d9ec3b5f68b6be7a50f6c4868539a7518
Jose Garcia <jogarcia@tuta.io>
2022-03-25 11:11:01 -0400

    Create basic product details

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductService } from '../product.service';
+import { CartService } from '../cart.service';
+
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product = {
+    id: 0,
+    description: '',
+    name: '',
+    price: '',
+  };
+
+  constructor(
+    private productService: ProductService,
+    public cartService: CartService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const productId = params['productId'];
+      this.findProduct(productId);
+    });
+  }
+
+  findProduct(productId: number) {
+    this.product =
+      this.productService
+        .getFeaturedProducts()
+        .find((product) => product.id === productId) ?? this.product;
+  }
+}
