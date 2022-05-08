e22730bc5c026edbb3e1d02179afe4c4dfb009cb
Jose Garcia <jogarcia@tuta.io>
2022-05-08 15:56:51 -0400

    Ref/ Add amount for each product to services

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,27 +1,45 @@
 import { Injectable } from '@angular/core';
 
 import { Product } from './product';
+import { CartProduct } from './cart-product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  cartProducts: Product[] = [];
+  cartProducts: CartProduct[] = [];
+  products: Product[] = [];
   accumulatedPrice: number = 0;
 
   constructor() {}
 
   setProduct(newProduct: Product): void {
-    this.cartProducts.push(newProduct);
+    if (this.products.includes(newProduct)) {
+      const index = this.getIndexOfCartProduct(newProduct);
+      this.cartProducts[index].amount++;
+    } else {
+      this.products.push(newProduct);
+      this.cartProducts.push({ product: newProduct, amount: 1 });
+    }
     this.recalculatePrice();
+  }
+
+  getIndexOfCartProduct(product: Product): number {
+    let indexOfProduct;
+    this.cartProducts.forEach((cartProduct, index) => {
+      if (cartProduct.product === product) {
+        indexOfProduct = index;
+      }
+    });
+    return indexOfProduct || -1;
   }
 
   deleteProduct(index: number) {
-    this.cartProducts.splice(index, 1);
+    this.products.splice(index, 1);
     this.recalculatePrice();
   }
 
-  getProducts(): Product[] {
+  getProducts(): CartProduct[] {
     return this.cartProducts;
   }
 
@@ -29,9 +47,9 @@ export class CartService {
     this.accumulatedPrice = 0;
     this.cartProducts.forEach((cartProduct) => {
       let price: number = 0;
-      typeof cartProduct.price === 'number'
-        ? (price = cartProduct.price)
-        : (price = parseFloat(cartProduct.price));
+      typeof cartProduct.product.price === 'number'
+        ? (price = cartProduct.product.price * cartProduct.amount)
+        : (price = parseFloat(cartProduct.product.price) * cartProduct.amount);
       this.accumulatedPrice += price;
     });
   }
