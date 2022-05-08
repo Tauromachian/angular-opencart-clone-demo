6fafadb497950fe399f774047e3548bfa275a227
Jose Garcia <jogarcia@tuta.io>
2022-05-08 09:48:34 -0400

    Start mockup server if not on production

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { makeServer } from './mock/server';
+
 if (environment.production) {
   enableProdMode();
+} else {
+  makeServer();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
