750de78c6d2d9c532a55b4a85922b8dbac20d548
Jose Garcia <jogarcia@tuta.io>
2022-05-08 15:57:19 -0400

    Use the new cartProducts

--- src/app/view-cart/view-cart.component.ts
+++ src/app/view-cart/view-cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../product';
+import { CartProduct } from '../cart-product';
 import { CartService } from '../cart.service';
 import { ProductService } from '../product.service';
 
@@ -10,7 +11,7 @@ import { ProductService } from '../product.service';
   styleUrls: ['./view-cart.component.scss'],
 })
 export class ViewCartComponent implements OnInit {
-  cartProducts: Product[] = [];
+  cartProducts: CartProduct[] = [];
   featuredProducts: Product[] = [];
 
   constructor(
