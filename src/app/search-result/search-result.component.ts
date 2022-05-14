9c85dac2fc3b74d0787a605571d556e899f6ffc1
Jose Garcia <jogarcia@tuta.io>
2022-05-14 15:56:02 -0400

    Use getProducts method

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss']
+  styleUrls: ['./search-result.component.scss'],
 })
 export class SearchResultComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getProducts(searchText: string): void {
+    this.productService
+      .getProductsBySearch(searchText)
+      .subscribe({ next: (products) => (this.products = products) });
   }
-
 }
